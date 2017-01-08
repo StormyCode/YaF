d180a2252e06af159645be57d87664ae00e20fef
J0th4 <julian.heep@gmail.com>
2017-01-08 19:00:25 +0100

    initial commit YaF.Data

---
+++ YaF/Data/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Data
+{
+    class Movie
+    {
+        public Movie(string directoryPath)
+        {
+            this.Directory = new DirectoryInfo(directoryPath);
+        }
+
+        private DirectoryInfo Directory { get; set; }
+        public string Name { get { return this.Directory.Name; } }
+        public double Size { get { return this.Directory.GetFiles("*", SearchOption.AllDirectories).Sum(x => x.Length); } }
+    }
+}
