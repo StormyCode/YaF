13fa9d9974bcb53da849c93a66fc5efeb7e2dfea
J0th4 <julian.heep@gmail.com>
2017-01-17 20:06:47 +0100

    issue #1

--- YaF/Controller/MediaLibrary.cs
+++ YaF/Controller/MediaLibrary.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data;
+using System.IO;
 
 namespace Controller
 {
-    public class Class1
+    public class MediaLibrary
     {
+        public List<Movie> Movies { get; private set; }
+
+        public MediaLibrary()
+        {
+            this.Movies = new List<Movie>();
+        }
+
+        public void AddMovies(string directory)
+        {
+            foreach (DirectoryInfo sub in new DirectoryInfo(directory).GetDirectories("*", SearchOption.TopDirectoryOnly))
+            {
+                this.Movies.Add(new Movie(sub.FullName));
+            }
+        }
     }
 }
